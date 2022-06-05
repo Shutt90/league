863c417c82eafbbd7f4d502bde43c9a183a429fa
Shutt90 <78647249+Shutt90@users.noreply.github.com>
2022-06-05 15:35:44 +0100

    Looping through for champion mastery

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
-	"strconv"
+	"sort"
 
 	"github.com/KnutZuidema/golio"
 	"github.com/KnutZuidema/golio/api"
@@ -17,23 +17,23 @@ func main() {
 	client := golio.NewClient(key,
 		golio.WithRegion(api.RegionEuropeWest),
 		golio.WithLogger(logrus.New().WithField("foo", "bar")))
-	rotation, err := client.Riot.LoL.Champion.GetFreeRotation()
-	for _, champ := range rotation.FreeChampionIDs {
-		champStringId := strconv.Itoa(champ)
 
-		champname, _ := client.DataDragon.GetChampionByID(champStringId)
-		fmt.Printf("%v\n", champname.Name)
-
-	}
-
-	summoner, _ := client.Riot.Summoner.GetByName("Shutt90")
+	summoner, _ := client.Riot.LoL.Summoner.GetByName("Shutt90")
 	fmt.Printf("%s is a level %d summoner\n", summoner.Name, summoner.SummonerLevel)
-	champion, _ := client.DataDragon.GetChampion("Jhin")
-	mastery, err := client.Riot.ChampionMastery.Get(summoner.ID, champion.Key)
-	if err != nil {
-		fmt.Printf("%s has not played any games on %s\n", summoner.Name, champion.Name)
-	} else {
-		fmt.Printf("%s has mastery level %d with %d points on %s\n", summoner.Name, mastery.ChampionLevel,
-			mastery.ChampionPoints, champion.Name)
+
+	allChamps, _ := client.DataDragon.GetChampions()
+
+	for _, champ := range allChamps {
+
+		currentChamp, err := client.Riot.LoL.ChampionMastery.Get(summoner.ID, champ.Key)
+
+		if err != nil {
+			fmt.Printf("Error: %v\n", err)
+			continue
+		}
+
+		fmt.Printf("Champion Name: %v\n Current Mastery: %v\n", champ.ID, currentChamp.ChampionPoints)
+
 	}
+
 }
